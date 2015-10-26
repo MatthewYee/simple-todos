e87ed034d3623698933dde0772ecb9ccab0615ad
Matthew Yee <mgy@hawaii.edu>
2015-10-25 16:21:54 -1000

    JSDocumentation and JSHint standard

--- simple-todos.js
+++ simple-todos.js
@@ -1,13 +1,30 @@
+/**
+ *
+ * By : Matthew Yee
+ * Credit to Meteor and their sample code for trying it out
+ */
+/*global Meteor, Template, Session*/
+/*exported  */
+
+/**
+ * This function focus on the Client side. Checks if the client is available and if it is then set up a session
+ */
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
+
+  /**
+   * Once Client is confirmed and the counter is created the session gets counter value
+   */
 
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
     }
   });
-
+  /**
+   * Focuse on the button interaction
+   */
   Template.hello.events({
     'click button': function () {
       // increment the counter when button is clicked
@@ -15,6 +32,10 @@ if (Meteor.isClient) {
     }
   });
 }
+
+/**
+ * Checks the server side and only starts up the server side
+ */
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
